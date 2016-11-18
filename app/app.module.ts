441c42dfe831a1205f7e185bb4f03a25b6d195b9
ivanbtrujillo <ivanbtrujillo@gmail.com>
2016-11-18 17:51:15 +0000

    Adds an inmemory fake-api and their crud methods

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+//InMemoryWebAPI Fake API
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard.component';
@@ -17,6 +22,8 @@ import { AppRoutingModule } from './app-routing.module';
     imports: [
       BrowserModule,
       FormsModule,
+      HttpModule,
+      InMemoryWebApiModule.forRoot(InMemoryDataService),
       AppRoutingModule,
     ],
     declarations: [
